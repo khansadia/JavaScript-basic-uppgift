1dac6f71e4e7230271e7accbe06389a1c204eb04
Sadia Khan <khansadia220@gmail.com>
2024-01-10 14:36:53 +0100

    update the README.md starting with Q1

---
+++ index.js
@@ -0,0 +1,6 @@
+// Write out Hello World! in the console.
+console.log("Hello World");
+
+1-Declare a variable age and assign your age to it. Write it out in the console together with a description of the age. Like, "this is my age...".
+const age = 35;
+console.log("This is my age:", age);
