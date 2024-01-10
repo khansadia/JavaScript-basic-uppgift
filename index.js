9755df43c13a2dd82cfb385e3444437e6f9249e2
Sadia Khan <khansadia220@gmail.com>
2024-01-10 15:02:40 +0100

    update index.js Solution Q2

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Write out Hello World! in the console.
 console.log("Hello World");
 
-1-Declare a variable age and assign your age to it. Write it out in the console together with a description of the age. Like, "this is my age...".
+//1-Declare a variable age and assign your age to it. Write it out in the console together with a description of the age. Like, "this is my age...".
 const age = 35;
 console.log("This is my age:", age);
+
+// 2-Declare three variables using the three different keywords that we have learned. Write them all out in the console. What was the different between these keywords?
+var varVariable = "A var variable"; //function-scoped and it can be reassigned
+const constVariable = "A const variable"; // block-scoped and it can't be reassigned
+let letVariable = "A let variable"; //block-scoped and it can be reassigned
+console.log(varVariable, constVariable, letVariable,);
